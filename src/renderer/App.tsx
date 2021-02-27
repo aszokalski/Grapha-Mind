ca526abfc12f045f53ea3920219105c46d3fe595
Igor Dmochowski <igor.dmochowski@gmail.com>
2021-02-28 00:22:45 +0100

    added account handling functions

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -29,7 +29,7 @@ import {SplashScreen} from './screens/SplashScreen';
 import './styles/App.css';
 import { DraftsTwoTone } from '@material-ui/icons';
 
-import { download, modify, add, remove, check_cred} from '../server';
+import { download, modify, add, remove, check_cred, create_user, change_password, activate_license, } from '../server';
 /**
  * Use a linkDataArray since we'll be using a GraphLinksModel,
  * and modelData for demonstration purposes. Note, though, that
@@ -88,11 +88,11 @@ class App extends React.Component<{}, AppState> {
       saved: false,
       path: null,
     }; 
-    console.log(check_cred('igor.dmochowski@gmail.com','haselko'));
+    activate_license('igor.dmochowski@gmail.com','nowehaslo2',2);
     //initiate graph object in backend and set unique graphId for the workplace
     download('').then(data =>{
       this.setState(
-        produce((draft: Appstate) => {//ten error kurwa skąd
+        produce((draft: AppState) => {//ten error kurwa skąd
           draft.graphId = data._id.toString();
           var dymki = data.nodes;
           for(let node of dymki){
--- src/server.tsx
+++ src/server.tsx
@@ -1,4 +1,4 @@
-import { KeyboardReturn } from "@material-ui/icons";
+import { DateRangeSharp } from "@material-ui/icons";
 
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectID } = require('mongodb').ObjectID;
@@ -92,18 +92,23 @@ export async function check_cred(email: string, password: string){
     const uri = "mongodb+srv://testuser:kosmatohuj@1mind.z6d3c.mongodb.net/1mind?retryWrites=true&w=majority";
     const client = new MongoClient(uri,{ useUnifiedTopology: true });
     
-    const query={'email': email};
+    const query={'email': email, 'password': password};
 
     try{
         await client.connect();
         const database = client.db('1mind')
         const collection = database.collection('users');
 
-        const ans = await collection.findOne(query).then((res: any) =>{
-            console.log(res);
-            //return res;
-        });
-        return ans;
+        const ans = await collection.findOne(query);
+        if(ans === null){
+            return false;
+        }
+        else if(typeof ans === 'object'){
+            return true;
+        }
+        else{
+            return 'unidentified error with logging in procedure';
+        }
     }
     catch(err){
         console.error(err);
@@ -111,4 +116,129 @@ export async function check_cred(email: string, password: string){
     finally{
         await client.close();
     }
+}
+
+export async function create_user(email: string, password: string) {
+    const uri = "mongodb+srv://testuser:kosmatohuj@1mind.z6d3c.mongodb.net/1mind?retryWrites=true&w=majority";
+    const client = new MongoClient(uri,{ useUnifiedTopology: true });
+
+    const doc = {'email': email, 'password': password,'license': new Date(), 'workplaces': []};
+
+    try{
+        await client.connect();
+        const database = client.db('1mind')
+        const collection = database.collection('users');
+        await collection.findOne({'email': email}).then(async (res:any) =>{
+            if(res === null){
+                await collection.insertOne(doc);
+            }
+            else if(typeof res === 'object'){
+                return 'this email has already been used';
+            }
+            else{
+                return 'unidentified error with creating account procedure';
+            }
+        })
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.close();
+    }
+}
+
+export async function change_password(email: string, password: string, newpassword: string){
+    const uri = "mongodb+srv://testuser:kosmatohuj@1mind.z6d3c.mongodb.net/1mind?retryWrites=true&w=majority";
+    const client = new MongoClient(uri,{ useUnifiedTopology: true });
+
+    const filter = {'email': email, 'password': password};
+    const updateDoc={$set: {'password': newpassword}};
+    const settings={};
+
+    try{
+        await client.connect();
+        const database = client.db('1mind');
+        const collection = database.collection('users');
+
+        await collection.findOne({'email': email, 'password': password}).then(async (res:any) =>{
+            if(typeof res === 'object'){
+                await collection.updateOne(filter, updateDoc, settings);
+            }
+            else if(res === null){
+                return 'email or password are not correct';
+            }
+            else{
+                return 'unidentifier error during changing users password';
+            }
+        })
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.close();
+    }
+}
+
+export async function activate_license(email: string, password: string, time: number){
+    const uri = "mongodb+srv://testuser:kosmatohuj@1mind.z6d3c.mongodb.net/1mind?retryWrites=true&w=majority";
+    const client = new MongoClient(uri,{ useUnifiedTopology: true });
+
+    const filter = {'email': email, 'password': password};
+    var date = new Date();
+    date.setDate(date.getDate() + time);
+    const updateDoc={$set: {'license': date}};
+    const settings={};
+
+    try{
+        await client.connect();
+        const database = client.db('1mind');
+        const collection = database.collection('users');
+
+        await collection.findOne({'email': email, 'password': password}).then(async (res:any) =>{
+            if(typeof res === 'object'){
+                if(compareDate(res.license, new Date())===1){
+                    date.setDate(date.getDate() + time);
+                    await collection.updateOne(filter, updateDoc, settings);
+                    console.log(1);
+                    console.log(res.license)
+
+                }
+                else if(compareDate(res.license, new Date())===-1){
+                    await collection.updateOne(filter, updateDoc, settings);
+                    console.log(2);
+                }
+                else if(compareDate(res.license, new Date())===0){
+                    await collection.updateOne(filter, updateDoc, settings);
+                }
+                else{
+                    return 'unidentified error while adding license days'
+                }
+                
+                //await collection,updateOne(filter, updateDoc, settings)
+            }
+            else if(res === null){
+                return "email or password are not correct";
+            }
+            else{
+                return 'unidentified error while prolonging the license';
+            }
+        })
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.close();
+    }
+}
+
+function compareDate(date1: Date, date2: Date)
+{
+    let d1 = new Date(date1); let d2 = new Date(date2);
+    let same = d1.getTime() === d2.getTime();
+    if (same) return 0;
+    if (d1 > d2) return 1;
+    if (d1 < d2) return -1;
 }
